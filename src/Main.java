cbdbc520438324d94b9550bae37aa60a3a19b204
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-08-09 01:16:35 +0300

    Homework_8_

--- src/Main.java
+++ src/Main.java
@@ -34,58 +34,81 @@ public class Main {
         System.out.println(arbitrarily[2]);
 
         System.out.println("Задача №2");
-        int[] weight2 = new int[3];
+        int[] weight2 = new int[]{1, 2, 3};
         weight2[0] = 1;
-        weight2[1] = 2;
-        weight2[2] = 3;
-        System.out.print(weight[0] + ", " + weight[1] + ", " + weight[2]);
+        for (int ind = 0; ind < weight2.length; ind++) {
+            if (ind == weight2.length - 1) {
+                System.out.println(weight2[2]);
+                break;
+            }
+
+            System.out.print(weight2[ind] + ", ");
+        }
         System.out.println();
 
         float[] bigBox2 = {1.57f, 7.654f, 9.986f};
-        System.out.print(bigBox2[0] + ", " + bigBox2[1] + ", " + bigBox2[2]);
-        System.out.println();
-
-        int[] arbitrarily2 = new int[7];
-        arbitrarily2[0] = 130;
-        arbitrarily2[1] = 135;
-        arbitrarily2[2] = 150;
-        arbitrarily2[3] = 100;
-        arbitrarily2[4] = 120;
-        arbitrarily2[5] = 170;
-        arbitrarily2[6] = 157;
-        System.out.println(arbitrarily2[0] + ", " + arbitrarily2[1] + ", " + arbitrarily2[2] + ", " + arbitrarily2[3] + ", " + arbitrarily2[4] + ", " + arbitrarily2[5] + ", " + arbitrarily2[6]);
-        System.out.println();
-        System.out.println("Задача №3");
-        int[] box = new int[3];
-        box[0] = 1;
-        box[1] = 2;
-        box[2] = 3;
-        for (int i = 3; i > 0; i--) {
-            System.out.print(i + ",");
+        bigBox2[0] = 1.57f;
+        for (int i = 0; i < bigBox2.length; i++) {
+            if (i == bigBox2.length - 1) {
+                System.out.println(bigBox2[2]);
+                break;
+            }
+            System.out.print(bigBox2[i] + ", ");
         }
         System.out.println();
-        float[] bigBox3 = {1.57f, 7.654f, 9.986f};
-        System.out.print(bigBox3[2] + ", " + bigBox3[1] + ", " + bigBox3[0]);
 
+        int[] arbitrarily2 = new int[5];
+        arbitrarily2[0] = 8;
+        arbitrarily2[1] = 37;
+        arbitrarily2[2] = 18;
+        arbitrarily2[3] = 45;
+        arbitrarily2[4] = 64;
+        for (int i = 0; i < arbitrarily2.length; i++) {
+            if (i == arbitrarily2.length - 1) {
+                System.out.println(arbitrarily2[4]);
+                break;
+            }
+            System.out.print(arbitrarily2[i] + ", ");
+        }
         System.out.println();
-        int[] arbitrarily3 = new int[7];
-        arbitrarily3[0] = 130;
-        arbitrarily3[1] = 135;
-        arbitrarily3[2] = 150;
-        arbitrarily3[3] = 100;
-        arbitrarily3[4] = 120;
-        arbitrarily3[5] = 170;
-        arbitrarily3[6] = 157;
-        System.out.print(arbitrarily2[6] + ", " + arbitrarily2[5] + ", " + arbitrarily2[4] + ", " + arbitrarily2[3] + ", " + arbitrarily2[2] + ", " + arbitrarily2[1] + ", " + arbitrarily2[0]);
+        System.out.println("Задача №3");
+        for (int i = weight.length - 1; i >= 0; i--) {
+            if (i < weight[0]) {
+                System.out.println(weight[i]);
+                break;
+            }
+            System.out.print(weight[i] + ",");
+        }
+        System.out.println();
+        for (int i = bigBox.length - 1; i >= 0; i--) {
+            if (i <= bigBox[0] - 1) {
+                System.out.println(bigBox[i]);
+                break;
+            }
+            System.out.print(bigBox[i] + ",");
+        }
+        System.out.println();
+        for (int i = arbitrarily.length - 1; i >= 0; i--) {
+            if (i == arbitrarily[0]) {
+                System.out.println(arbitrarily[i]);
+                break;
+            }
+            System.out.print(arbitrarily[i] + ",");
+
+        }
         System.out.println();
         System.out.println("Задача №4");
-        int[] numbers = new int[]{10,7,15,94,18,6,9,37,36,14,25,33};
+        int[] numbers = new int[]{10, 7, 15, 94, 18, 6, 9, 37, 36, 14, 25, 33};
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println((numbers)[i]);
             if (numbers[i] % 2 != 0)
-                numbers[i] = numbers[i] + 1;}
-            System.out.println(Arrays.toString(numbers));
+                numbers[i] = numbers[i] + 1;
+
+
+        }
+        System.out.println(Arrays.toString(numbers));
 
 
     }
+
+
 }
